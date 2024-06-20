2b4b5cedad30457c77f1184856cb202ac9b1a92c
songminjjuk <alstjrdlep@naver.com>
2024-06-20 15:50:59 +0000

    update index

--- frontend/src/components/HeaderComponent.jsx
+++ frontend/src/components/HeaderComponent.jsx
@@ -12,8 +12,8 @@ export default class HeaderComponent extends Component {
         <header>
           <nav className="navbar navbar-expand-md navbar-dark bg-dark">
             <div>
-              <a href="http://www.fastcampus-dragon.net" className="navbar-brand">
-                kimdragon - jenkins
+              <a href="http://ecs-front-alb-506385164.ap-northeast-2.elb.amazonaws.com" className="navbar-brand">
+                songminseok - jenkins
               </a>
             </div>
           </nav>
