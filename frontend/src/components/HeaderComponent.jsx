7481d11712388060a3e185ed12126f502632fee0
songminjjuk <alstjrdlep@naver.com>
2024-06-21 05:12:37 +0000

    test

--- frontend/src/components/HeaderComponent.jsx
+++ frontend/src/components/HeaderComponent.jsx
@@ -13,7 +13,7 @@ export default class HeaderComponent extends Component {
           <nav className="navbar navbar-expand-md navbar-dark bg-dark">
             <div>
               <a href="http://www.zinucha0099.shop" className="navbar-brand">
-                songminseok - jenkins - 시연
+                songminseok
               </a>
             </div>
           </nav>
